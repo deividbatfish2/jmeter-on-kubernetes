cc1c90288fe354121c44657e0184b12236ab2b00
Deivid Teixeira <deividbatfish2@gmail.com>
2021-03-18 20:33:54 -0300

    test: ensure LoadProjectController returns 201 if all required fields are provided

--- src/presentation/controller/load-project-controller.spec.ts
+++ src/presentation/controller/load-project-controller.spec.ts
@@ -1,15 +1,21 @@
 import { LoadProjectController } from './load-project-controller'
 
 describe('LoadProjectController', () => {
-  test('Should return 400 if name is not provided', async () => {
+  test('should return 400 if name is not provided', async () => {
     const sut = new LoadProjectController()
-    const result = await sut.handle({ body: { description: 'any_description' } })
+    const result = await sut.handle({ body: { description: 'Any description' } })
     expect(result.statusCode).toBe(400)
   })
 
-  test('Should return 400 if description is not provided', async () => {
+  test('should return 400 if description is not provided', async () => {
     const sut = new LoadProjectController()
-    const result = await sut.handle({ body: { name: 'any_name' } })
+    const result = await sut.handle({ body: { name: 'Any Name' } })
     expect(result.statusCode).toBe(400)
+  })
+
+  test('should return 201 if all required fields are informed', async () => {
+    const sut = new LoadProjectController()
+    const result = await sut.handle({ body: { name: 'Any Name', description: 'Any description' } })
+    expect(result.statusCode).toBe(201)
   })
 })
