7dfb9b14ff030d96e7fcc92ce9317ef909650745
Deivid Teixeira <deividbatfish2@gmail.com>
2021-03-18 20:15:22 -0300

    feat: ensure LoadProjectController returns 400 if description is not provided

--- src/presentation/controller/load-project-controller.spec.ts
+++ src/presentation/controller/load-project-controller.spec.ts
@@ -6,4 +6,10 @@ describe('LoadProjectController', () => {
     const result = await sut.handle({ body: { description: 'any_description' } })
     expect(result.statusCode).toBe(400)
   })
+
+  test('Should return 400 if description is not provided', async () => {
+    const sut = new LoadProjectController()
+    const result = await sut.handle({ body: { name: 'any_name' } })
+    expect(result.statusCode).toBe(400)
+  })
 })
