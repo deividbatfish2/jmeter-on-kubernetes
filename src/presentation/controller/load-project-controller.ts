e2edb85aa501b403a2420c2bd779287d6c6afd4b
Deivid Teixeira <deividbatfish2@gmail.com>
2021-03-18 20:42:05 -0300

    feat: ensure LoadProjectController returns 400 if jmxProvider is not provided

--- src/presentation/controller/load-project-controller.ts
+++ src/presentation/controller/load-project-controller.ts
@@ -2,8 +2,8 @@ import { HttpRequest, HttpResponse } from '../proptocols/http'
 
 export class LoadProjectController {
   async handle (req: HttpRequest): Promise<HttpResponse> {
-    const { name, description } = req.body
-    if (!name || !description) { return { statusCode: 400 } }
+    const { name, description, jmxProvider } = req.body
+    if (!name || !description || !jmxProvider) { return { statusCode: 400 } }
 
     return { statusCode: 201 }
   }
