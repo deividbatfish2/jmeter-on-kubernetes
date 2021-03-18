39b19fbc43f2cd9150de497726cca61785d68f60
Deivid Teixeira <deividbatfish2@gmail.com>
2021-03-18 20:42:22 -0300

    test: ensure LoadProjectController returns 400 if jmxProvider is not provided

--- src/presentation/controller/load-project-controller.spec.ts
+++ src/presentation/controller/load-project-controller.spec.ts
@@ -13,9 +13,15 @@ describe('LoadProjectController', () => {
     expect(result.statusCode).toBe(400)
   })
 
-  test('should return 201 if all required fields are informed', async () => {
+  test('should return 400 if jmxProvider is not provided', async () => {
     const sut = new LoadProjectController()
     const result = await sut.handle({ body: { name: 'Any Name', description: 'Any description' } })
+    expect(result.statusCode).toBe(400)
+  })
+
+  test('should return 201 if all required fields are informed', async () => {
+    const sut = new LoadProjectController()
+    const result = await sut.handle({ body: { name: 'Any Name', description: 'Any description', jmxProvider: 'any_provider' } })
     expect(result.statusCode).toBe(201)
   })
 })
