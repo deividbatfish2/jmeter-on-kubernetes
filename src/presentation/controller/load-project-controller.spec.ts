1b7e608cdfab9b9f0133e005fe1f1563a5c804fa
Deivid Teixeira <deividbatfish2@gmail.com>
2021-03-18 20:52:01 -0300

    refactor: move sut creation to a factory method helper

--- src/presentation/controller/load-project-controller.spec.ts
+++ src/presentation/controller/load-project-controller.spec.ts
@@ -1,26 +1,30 @@
 import { LoadProjectController } from './load-project-controller'
 
+const makeSut = (): LoadProjectController => {
+  return new LoadProjectController()
+}
+
 describe('LoadProjectController', () => {
   test('should return 400 if name is not provided', async () => {
-    const sut = new LoadProjectController()
+    const sut = makeSut()
     const result = await sut.handle({ body: { description: 'Any description' } })
     expect(result.statusCode).toBe(400)
   })
 
   test('should return 400 if description is not provided', async () => {
-    const sut = new LoadProjectController()
+    const sut = makeSut()
     const result = await sut.handle({ body: { name: 'Any Name' } })
     expect(result.statusCode).toBe(400)
   })
 
   test('should return 400 if jmxProvider is not provided', async () => {
-    const sut = new LoadProjectController()
+    const sut = makeSut()
     const result = await sut.handle({ body: { name: 'Any Name', description: 'Any description' } })
     expect(result.statusCode).toBe(400)
   })
 
   test('should return 201 if all required fields are informed', async () => {
-    const sut = new LoadProjectController()
+    const sut = makeSut()
     const result = await sut.handle({ body: { name: 'Any Name', description: 'Any description', jmxProvider: 'any_provider' } })
     expect(result.statusCode).toBe(201)
   })
