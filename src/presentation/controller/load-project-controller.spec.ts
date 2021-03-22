0fe2f0c5fe9ed123b5226bf31927d9151b91c951
Deivid Teixeira <deividbatfish2@gmail.com>
2021-03-22 19:21:32 -0300

    refactor: add return type to factory

--- src/presentation/controller/load-project-controller.spec.ts
+++ src/presentation/controller/load-project-controller.spec.ts
@@ -1,5 +1,9 @@
 import { LoadProjectController } from './load-project-controller'
 
+const makeFakeRequest = (): any => ({
+  body: { name: 'Any Name', description: 'Any description', jmxProvider: 'any_provider' }
+})
+
 const makeSut = (): LoadProjectController => {
   return new LoadProjectController()
 }
@@ -7,25 +11,28 @@ const makeSut = (): LoadProjectController => {
 describe('LoadProjectController', () => {
   test('should return 400 if name is not provided', async () => {
     const sut = makeSut()
-    const result = await sut.handle({ body: { description: 'Any description' } })
+    const { body: name, ...requestWithoutName } = makeFakeRequest()
+    const result = await sut.handle({ body: requestWithoutName })
     expect(result.statusCode).toBe(400)
   })
 
   test('should return 400 if description is not provided', async () => {
     const sut = makeSut()
-    const result = await sut.handle({ body: { name: 'Any Name' } })
+    const { body: description, ...requestWithoutDescription } = makeFakeRequest()
+    const result = await sut.handle({ body: requestWithoutDescription })
     expect(result.statusCode).toBe(400)
   })
 
   test('should return 400 if jmxProvider is not provided', async () => {
     const sut = makeSut()
-    const result = await sut.handle({ body: { name: 'Any Name', description: 'Any description' } })
+    const { body: jmxProvider, ...requestWithoutJmxProvider } = makeFakeRequest()
+    const result = await sut.handle({ body: requestWithoutJmxProvider })
     expect(result.statusCode).toBe(400)
   })
 
   test('should return 201 if all required fields are informed', async () => {
     const sut = makeSut()
-    const result = await sut.handle({ body: { name: 'Any Name', description: 'Any description', jmxProvider: 'any_provider' } })
+    const result = await sut.handle(makeFakeRequest())
     expect(result.statusCode).toBe(201)
   })
 })
