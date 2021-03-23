7739df4dd2641431733028f425f5e2dd60d0b8d4
Deivid Teixeira <deividbatfish2@gmail.com>
2021-03-22 21:11:31 -0300

    feat: ensure LoadProjectController returns 400 if command is not provided

--- src/presentation/controller/load-project-controller.ts
+++ src/presentation/controller/load-project-controller.ts
@@ -2,8 +2,8 @@ import { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class LoadProjectController {
   async handle (req: HttpRequest): Promise<HttpResponse> {
-    const { name, description, jmxProvider } = req.body
-    if (!name || !description || !jmxProvider) { return { statusCode: 400 } }
+    const { name, description, jmxProvider, command } = req.body
+    if (!name || !description || !jmxProvider || !command) { return { statusCode: 400 } }
 
     return { statusCode: 201 }
   }
