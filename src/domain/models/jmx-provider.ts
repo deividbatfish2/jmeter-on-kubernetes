4bb05ffafbdd25856aa2d47bb10c0fccf535794a
Deivid Teixeira <deividbatfish2@gmail.com>
2021-03-22 22:12:21 -0300

    feat: ensure LoadProjectController returns 400 if jmxProvider is not recognized

--- src/domain/models/jmx-provider.ts
+++ src/domain/models/jmx-provider.ts
@@ -1,3 +1,3 @@
 export enum JmxProvider {
-  GIT
+  GIT = 'git'
 }
--- src/presentation/controller/load-project-controller.ts
+++ src/presentation/controller/load-project-controller.ts
@@ -1,3 +1,4 @@
+import { JmxProvider } from '../../domain/models/jmx-provider'
 import { AddLoadProject } from '../../domain/usecases/load-project/add-load-project'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 
@@ -7,6 +8,11 @@ export class LoadProjectController {
   async handle (req: HttpRequest): Promise<HttpResponse> {
     const { name, description, jmxProvider, command } = req.body
     if (!name || !description || !jmxProvider || !command) { return { statusCode: 400 } }
+
+    console.log(jmxProvider)
+    console.log(Object.values(JmxProvider))
+
+    if (!Object.values(JmxProvider).includes(jmxProvider)) { return { statusCode: 400 } }
 
     try {
       await this.addLoadProject.add({ name, description, jmxProvider, command })
