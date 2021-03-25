75997913b22b492710fb2c383e1ae2109236bd03
Deivid Teixeira <deividbatfish2@gmail.com>
2021-03-25 19:50:35 -0300

    refactor: move sut creation to a factory helper method

--- src/db/usecases/add-load-project/db-add-load-project.spec.ts
+++ src/db/usecases/add-load-project/db-add-load-project.spec.ts
@@ -2,38 +2,53 @@ import { AddLoadProjectModel, JmxProvider, LoadProjectModel } from '../../../pre
 import { AddLoadProjectRepository } from '../../protocols/load-project/add-load-project-repository'
 import { DbAddLoadProject } from './db-add-load-project'
 
+const makeFakeLoadProjectModel = (): LoadProjectModel => ({
+  id: 'any_id',
+  name: 'Any Name',
+  description: 'Any description',
+  jmxProvider: JmxProvider.GIT,
+  command: 'any command'
+})
+
+const makeFakeAddLoadProjectModel = (): AddLoadProjectModel => ({
+  name: 'Any Name',
+  description: 'Any description',
+  jmxProvider: JmxProvider.GIT,
+  command: 'any command'
+})
+
+const makeAddLoadProjectRepositoryStub = (): AddLoadProjectRepository => {
+  class AddLoadProjectRepositoryStub implements AddLoadProjectRepository {
+    async add (addLoadProject: AddLoadProjectModel): Promise<LoadProjectModel> {
+      return makeFakeLoadProjectModel()
+    }
+  }
+  return new AddLoadProjectRepositoryStub()
+}
+
+interface SutTypes {
+  addLoadProjectRepositoryStub: AddLoadProjectRepository
+  sut: DbAddLoadProject
+}
+
+const makeSut = (): SutTypes => {
+  const addLoadProjectRepositoryStub = makeAddLoadProjectRepositoryStub()
+  const sut = new DbAddLoadProject(addLoadProjectRepositoryStub)
+
+  return {
+    sut, addLoadProjectRepositoryStub
+  }
+}
+
 describe('DbAddLoadProject UseCase', () => {
   test('Should call AddLoadProjectRepository with correct values', async () => {
-    class AddLoadProjectRepositoryStub implements AddLoadProjectRepository {
-      async add (addLoadProject: AddLoadProjectModel): Promise<LoadProjectModel> {
-        return {
-          id: 'any_id',
-          name: 'Any Name',
-          description: 'Any description',
-          jmxProvider: JmxProvider.GIT,
-          command: 'any command'
-        }
-      }
-    }
-
-    const addLoadProjectRepositoryStub = new AddLoadProjectRepositoryStub()
-    const sut = new DbAddLoadProject(addLoadProjectRepositoryStub)
+    const { sut, addLoadProjectRepositoryStub } = makeSut()
 
     const addStub = jest.spyOn(addLoadProjectRepositoryStub, 'add')
 
-    await sut.add({
-      name: 'Any Name',
-      description: 'Any description',
-      jmxProvider: JmxProvider.GIT,
-      command: 'any command'
-    })
+    await sut.add(makeFakeAddLoadProjectModel())
 
-    expect(addStub).toHaveBeenCalledWith({
-      name: 'Any Name',
-      description: 'Any description',
-      jmxProvider: JmxProvider.GIT,
-      command: 'any command'
-    })
+    expect(addStub).toHaveBeenCalledWith(makeFakeAddLoadProjectModel())
   })
 
   test.todo('Should throws if AddLoadProjectRepository throws')
