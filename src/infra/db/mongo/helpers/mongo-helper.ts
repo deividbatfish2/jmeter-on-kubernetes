68536bcb4ec8b91dd63084ca047c3633789af172
Deivid Teixeira <deividbatfish2@gmail.com>
2021-03-25 21:01:30 -0300

    feat: ensure MongoHelper reconnect if mongo disconnect

---
+++ src/infra/db/mongo/helpers/mongo-helper.ts
@@ -0,0 +1,32 @@
+import { MongoClient, Collection } from 'mongodb'
+
+export const MongoHelper = {
+  client: null as unknown as MongoClient,
+  uri: null as unknown as string,
+
+  async connect (uri: string): Promise<void> {
+    this.uri = uri
+    this.client = await MongoClient.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  },
+
+  async disconnect (): Promise<void> {
+    await this.client.close()
+    this.client = null as unknown as MongoClient
+  },
+
+  async getCollection (name: string): Promise<Collection> {
+    if (!this.client?.isConnected()) {
+      await this.connect(this.uri)
+    }
+    return this.client.db().collection(name)
+  },
+
+  map<T>(collection: any): T {
+    const { _id, ...collectionWithoutId } = collection
+
+    return { ...collectionWithoutId, ...{ id: _id } }
+  }
+}
