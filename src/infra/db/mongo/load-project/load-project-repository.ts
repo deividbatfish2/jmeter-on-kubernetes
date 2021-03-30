4e9ef938bb7da0fe5d0a9686073875a0c89564e0
Deivid Teixeira <deividbatfish2@gmail.com>
2021-03-30 12:39:01 -0300

    feat: ensure LoadProjectMongoRepository return an load project on find by name

--- src/infra/db/mongo/load-project/load-project-repository.ts
+++ src/infra/db/mongo/load-project/load-project-repository.ts
@@ -1,10 +1,17 @@
 import { AddLoadProjectRepository } from '../../../../data/protocols/load-project/add-load-project-repository'
+import { FindLoadProjectByNameRepository } from '../../../../data/protocols/load-project/FindLoadProjectByNameRepository'
 import { LoadProjectModel } from '../../../../domain/models/load-project-model'
 import { AddLoadProjectModel } from '../../../../presentation/controller/load-project-controller-protocols'
 import { Collections } from '../helpers/collections'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class LoadProjectMongoRepository implements AddLoadProjectRepository {
+export class LoadProjectMongoRepository implements AddLoadProjectRepository, FindLoadProjectByNameRepository {
+  async findLoadProjectByName (name: string): Promise<LoadProjectModel> {
+    const loadProjectCollection = await MongoHelper.getCollection(Collections.loadProject)
+    const loadProject = await loadProjectCollection.findOne({ name })
+    return MongoHelper.map<LoadProjectModel>(loadProject)
+  }
+
   async add (addLoadProject: AddLoadProjectModel): Promise<LoadProjectModel> {
     const loadProjectCollection = await MongoHelper.getCollection(Collections.loadProject)
     const { ops: [loadProject] } = await loadProjectCollection.insertOne(addLoadProject)
