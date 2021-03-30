e66aa33e11570f13c458575169893c46f9c5c744
Deivid Teixeira <deividbatfish2@gmail.com>
2021-03-29 22:21:37 -0300

    test: ensure POST load-project create a load project

---
+++ src/main/routes/load-project.test.ts
@@ -0,0 +1,33 @@
+import request from 'supertest'
+import { Collections } from '../../infra/db/mongo/helpers/collections'
+import { MongoHelper } from '../../infra/db/mongo/helpers/mongo-helper'
+import app from '../config/app'
+
+describe('Load Project routes', () => {
+  let loadProjectCollection
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  beforeEach(async () => {
+    loadProjectCollection = await MongoHelper.getCollection(Collections.loadProject)
+    await loadProjectCollection.deleteMany({})
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+  describe('POST /api/load-projec', () => {
+    test('Should create a load project', async () => {
+      await request(app)
+        .post('/api/load-project')
+        .send({
+          name: 'Any Name',
+          description: 'Any Description',
+          jmxProvider: 'git',
+          command: 'any command'
+        })
+        .expect(201)
+    })
+  })
+})
