8a16a34f97833d6d97b02616b4c4711efedd6064
Deivid Teixeira <deividbatfish2@gmail.com>
2021-04-05 20:31:43 -0300

    refactor: move sut creation to a factory helper method

--- src/presentation/controller/health-check/health-check-controller.spec.ts
+++ src/presentation/controller/health-check/health-check-controller.spec.ts
@@ -2,16 +2,34 @@ import { DateHelper } from '../../utils/date/date-helper-protocol'
 import { ok } from '../../utils/http-responses'
 import { HealthCheckController } from './health-check-controller'
 
-describe('Health Check Controller', () => {
-  test('Should return 200 on success', async () => {
-    class DateHelperStub implements DateHelper {
-      now (): number {
-        return 1617664390886
-      }
+const makeDateHelperStub = (): DateHelper => {
+  class DateHelperStub implements DateHelper {
+    now (): number {
+      return 1617664390886
     }
-    const dateHelperStub = new DateHelperStub()
-    const sut = new HealthCheckController(dateHelperStub)
-    const result = await sut.handle({})
-    expect(result).toStrictEqual(ok({ timestamp: 1617664390886 }))
+  }
+  return new DateHelperStub()
+}
+
+interface SutTypes {
+  sut: HealthCheckController
+  dateHelperStub: DateHelper
+}
+
+const makeSut = (): SutTypes => {
+  const dateHelperStub = makeDateHelperStub()
+  const sut = new HealthCheckController(dateHelperStub)
+  return {
+    sut, dateHelperStub
+  }
+}
+
+describe('Health Check Controller', () => {
+  describe('Success', () => {
+    test('Should return 200 on success', async () => {
+      const { sut } = makeSut()
+      const result = await sut.handle({})
+      expect(result).toStrictEqual(ok({ timestamp: 1617664390886 }))
+    })
   })
 })
