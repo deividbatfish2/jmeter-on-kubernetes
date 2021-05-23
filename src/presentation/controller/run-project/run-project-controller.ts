0d9af35efdbb0598dc98d1f7187eeeb5f43198b6
Deivid Teixeira <deividbatfish2@gmail.com>
2021-05-23 13:52:09 -0300

    feat: ensure RunProjectController return server error if RunLoadProject throws

--- src/presentation/controller/run-project/run-project-controller.ts
+++ src/presentation/controller/run-project/run-project-controller.ts
@@ -1,7 +1,7 @@
 import { RunLoadProject } from '../../../domain/usecases/load-project/run-load-project'
 import { Controller } from '../../protocols/controller'
 import { Validation } from '../../protocols/validation'
-import { badRequest } from '../../utils/http-responses'
+import { badRequest, serverError } from '../../utils/http-responses'
 import { HttpRequest, HttpResponse } from '../load-project/load-project-controller-protocols'
 
 export class RunProjectController implements Controller {
@@ -11,15 +11,23 @@ export class RunProjectController implements Controller {
   ) { }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { body, path } = httpRequest
-    const validationResult = this.validationComposite.validate(body)
-    if (validationResult) {
-      return badRequest(validationResult)
+    try {
+      const { body, path } = httpRequest
+      const validationResult = this.validationComposite.validate(body)
+      if (validationResult) {
+        return badRequest(validationResult)
+      }
+      const loadProjectFails = await this.runLoadProject.run({
+        qtdRunners: body.qtdRunners as number,
+        idProject: path.idproject as string
+      })
+
+      if (loadProjectFails) {
+        return badRequest(loadProjectFails)
+      }
+      return null
+    } catch {
+      return serverError()
     }
-    await this.runLoadProject.run({
-      qtdRunners: body.qtdRunners as number,
-      idProject: path.idproject as string
-    })
-    return null
   }
 }
