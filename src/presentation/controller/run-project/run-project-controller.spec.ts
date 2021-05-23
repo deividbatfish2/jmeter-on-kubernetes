e2fb5bae6b6dc3336e34f476cde391e28b6fc446
Deivid Teixeira <deividbatfish2@gmail.com>
2021-05-23 13:29:38 -0300

    test: ensure RunProjectController calls RunLoadProject with correct values

--- src/presentation/controller/run-project/run-project-controller.spec.ts
+++ src/presentation/controller/run-project/run-project-controller.spec.ts
@@ -1,8 +1,17 @@
+import { RunLoadProject, RunLoadProjectModel } from '../../../domain/usecases/load-project/run-load-project'
 import { Validation } from '../../protocols/validation'
 import { badRequest } from '../../utils/http-responses'
 import { HttpRequest } from '../load-project/load-project-controller-protocols'
 import { RunProjectController } from './run-project-controller'
 
+const makeHttpRequest = (): HttpRequest => ({
+  body: {
+    qtdRunners: 2
+  },
+  path: {
+    idproject: 'any-id'
+  }
+})
 const makeValidation = (): Validation => {
   class ValidationStub implements Validation {
     validate (input: any): Error {
@@ -11,16 +20,27 @@ const makeValidation = (): Validation => {
   }
   return new ValidationStub()
 }
+
+const makeRunProjectUseCase = (): RunLoadProject => {
+  class RunProjectStub implements RunLoadProject {
+    async run (runLoadProjectModel: RunLoadProjectModel): Promise<Error> {
+      return null
+    }
+  }
+  return new RunProjectStub()
+}
 interface SutTypes {
   validation: Validation
+  runProjectStub: RunLoadProject
   runProjectController: RunProjectController
 }
 const makeSut = (): SutTypes => {
   const validation = makeValidation()
-  const runProjectController = new RunProjectController(validation)
+  const runProjectStub = makeRunProjectUseCase()
+  const runProjectController = new RunProjectController(validation, runProjectStub)
 
   return {
-    validation, runProjectController
+    runProjectController, validation, runProjectStub
   }
 }
 
@@ -31,11 +51,7 @@ describe('Run Project Controller', () => {
 
       const validateSpy = jest.spyOn(validation, 'validate')
 
-      const httpRequest: HttpRequest = {
-        body: {
-          anyField: 'any value'
-        }
-      }
+      const httpRequest = makeHttpRequest()
 
       await runProjectController.handle(httpRequest)
 
@@ -46,14 +62,24 @@ describe('Run Project Controller', () => {
       const { runProjectController, validation } = makeSut()
       jest.spyOn(validation, 'validate').mockImplementationOnce((input: any) => new Error('Any error'))
 
-      const httpRequest: HttpRequest = {
-        body: {
-          anyField: 'any value'
-        }
-      }
+      const httpRequest = makeHttpRequest()
 
       const result = await runProjectController.handle(httpRequest)
       expect(result).toStrictEqual(badRequest(new Error('Any error')))
+    })
+  })
+
+  describe('Run Load Project Use case', () => {
+    test('Should call RunLoadProject with correct values', async () => {
+      const { runProjectController, runProjectStub } = makeSut()
+      const runSpy = jest.spyOn(runProjectStub, 'run')
+      const httpRequest = makeHttpRequest()
+      await runProjectController.handle(httpRequest)
+
+      expect(runSpy).toHaveBeenLastCalledWith({
+        idProject: 'any-id',
+        qtdRunners: 2
+      })
     })
   })
 })
