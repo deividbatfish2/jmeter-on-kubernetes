9ae550409ca931150ea667b63b57822773329ea1
Deivid Teixeira <deividbatfish2@gmail.com>
2021-05-23 12:44:40 -0300

    refactor: move sut  creation to a helper method

--- src/presentation/controller/run-project/run-project-controller.spec.ts
+++ src/presentation/controller/run-project/run-project-controller.spec.ts
@@ -2,16 +2,31 @@ import { Validation } from '../../protocols/validation'
 import { HttpRequest } from '../load-project/load-project-controller-protocols'
 import { RunProjectController } from './run-project-controller'
 
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error {
+      return null
+    }
+  }
+  return new ValidationStub()
+}
+interface SutTypes {
+  validation: Validation
+  runProjectController: RunProjectController
+}
+const makeSut = (): SutTypes => {
+  const validation = makeValidation()
+  const runProjectController = new RunProjectController(validation)
+
+  return {
+    validation, runProjectController
+  }
+}
+
 describe('Run Project Controller', () => {
   describe('Valdations', () => {
     test('Should call validation with correct values', async () => {
-      class ValidationStub implements Validation {
-        validate (input: any): Error {
-          return null
-        }
-      }
-      const validation = new ValidationStub()
-      const runProjectController = new RunProjectController(validation)
+      const { runProjectController, validation } = makeSut()
 
       const validateSpy = jest.spyOn(validation, 'validate')
 
