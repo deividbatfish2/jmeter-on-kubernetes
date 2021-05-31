844e031482881b14c0f80518c73fcfb28a01dd32
Deivid Teixeira <deividbatfish2@gmail.com>
2021-05-31 20:58:30 -0300

    test: ensure DbRunLoadProject returns a ProjectInProgressError if project status equals to running

--- src/data/usecases/run/db-run-load-project.spec.ts
+++ src/data/usecases/run/db-run-load-project.spec.ts
@@ -1,23 +1,28 @@
+import { ProjectInProgressError } from '../../../domain/error/project-in-progress-error'
+import { ProjectNotFindedError } from '../../../domain/error/project-not-find-error'
 import { Provider } from '../../../domain/models/jmx-provider'
 import { LoadProjectModel } from '../../../domain/models/load-project-model'
 import { StatusProject } from '../../../domain/models/status-project'
 import { FindLoadProjectByIdRepository } from '../../protocols/load-project/find-load-project-by-id-repository'
 import { DbRunLoadProject } from './db-run-load-project'
 
+const makeDefaultReturnOfProject = (): LoadProjectModel => (
+  {
+    command: 'any command',
+    description: 'any description',
+    id: 'any_id',
+    jmxProvider: {
+      provider: Provider.GIT,
+      specificFields: {}
+    },
+    name: 'Any Project',
+    status: StatusProject.STOPED
+  }
+)
 const makeFindLoadProjectByIdRepositoryStub = (): FindLoadProjectByIdRepository => {
   class FindLoadProjectByIdRepositoryStub implements FindLoadProjectByIdRepository {
     async findLoadProjectById (id: string): Promise<LoadProjectModel> {
-      return {
-        command: 'any command',
-        description: 'any description',
-        id: 'any_id',
-        jmxProvider: {
-          provider: Provider.GIT,
-          specificFields: {}
-        },
-        name: 'Any Project',
-        status: StatusProject.STOPED
-      }
+      return makeDefaultReturnOfProject()
     }
   }
   return new FindLoadProjectByIdRepositoryStub()
@@ -44,7 +49,21 @@ describe('K8s Run Project', () => {
       expect(findLoadProjectByIdSpy).toHaveBeenCalledWith('any_id')
     })
 
-    test('should return a ProjectNotFindException if FindLoadProjectByIdRepository returns null', () => )
+    test('should return a ProjectNotFindError if FindLoadProjectByIdRepository returns null', async () => {
+      const { dbRunLoadProject, findLoadProjectByIdRepositoryStub } = makeSut()
+      jest.spyOn(findLoadProjectByIdRepositoryStub, 'findLoadProjectById')
+        .mockImplementationOnce(() => null)
+      const result = await dbRunLoadProject.run({ idProject: 'any_id', qtdRunners: 2 })
+      expect(result).toStrictEqual(new ProjectNotFindedError('any_id'))
+    })
+
+    test('should return a ProjectInProgressError if project status equals to running', async () => {
+      const { dbRunLoadProject, findLoadProjectByIdRepositoryStub } = makeSut()
+      jest.spyOn(findLoadProjectByIdRepositoryStub, 'findLoadProjectById')
+        .mockImplementationOnce(async () => ({ ...makeDefaultReturnOfProject(), ...{ status: StatusProject.RUNNING } }))
+      const result = await dbRunLoadProject.run({ idProject: 'any_id', qtdRunners: 2 })
+      expect(result).toStrictEqual(new ProjectInProgressError('any_id'))
+    })
 
     test('should throw if FindLoadProjectByIdRepository throws', async () => {
       const { dbRunLoadProject, findLoadProjectByIdRepositoryStub } = makeSut()
