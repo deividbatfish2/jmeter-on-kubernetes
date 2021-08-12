7769bed782ca48c17276a3955e2384f8ba76baf1
Deivid de Assis Teixeira <deividbatfish2@gmail.com>
2021-08-12 20:50:12 -0300

    test: ensure GitProvider throws if GitAdapter throws

--- src/infra/proivider/git/git-provider.spec.ts
+++ src/infra/proivider/git/git-provider.spec.ts
@@ -1,20 +1,45 @@
 import { GitAdapter, GitAdapterModel } from "../../protocols/git-adapter"
 import { GitProvider } from "./git-provider"
 
+const makeGitAdapterStub = (): GitAdapter => {
+    class GitAdaperStub implements GitAdapter {
+        async clone(projetcToClone: GitAdapterModel): Promise<void | Error> {
+            return
+        }
+    }
+    return new GitAdaperStub()
+}
+
+interface SutTypes {
+    gitAdapterStub: GitAdapter
+    sut: GitProvider
+}
+
+const makeSut = (): SutTypes => {
+    const gitAdapterStub = makeGitAdapterStub()
+    const sut = new GitProvider(gitAdapterStub)
+
+    return {
+        gitAdapterStub, sut
+    }
+}
 describe('GitProvider', () => {
     test('Should call GitAdapter with correct values', async () => {
-        class GitAdaperStub implements GitAdapter {
-            async clone(projetcToClone: GitAdapterModel): Promise<void | Error> {
-                return
-            }
-        }
-        const gitAdapterStub = new GitAdaperStub()
+
+        const { gitAdapterStub, sut } = makeSut()
         const cloneSpy = jest.spyOn(gitAdapterStub, 'clone')
 
-        const gitProvider = new GitProvider(gitAdapterStub)
-
-        await gitProvider.getProject({ path: 'https://any_path' })
+        await sut.getProject({ path: 'https://any_path' })
 
         expect(cloneSpy).toHaveBeenCalledWith({ path: 'https://any_path' })
+    })
+
+    test('Should throw if GitAdapter trows', async () => {
+        const { gitAdapterStub, sut } = makeSut()
+        jest.spyOn(gitAdapterStub, 'clone').mockImplementationOnce(() => { throw new Error('any error') })
+
+        const error = sut.getProject({ path: 'https://any_path' })
+
+        await expect(error).rejects.toThrowError('any error')
     })
 })
